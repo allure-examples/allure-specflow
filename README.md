4a8da04f0d8091fc9f2714022db29122f3df6b0c
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2023-10-18 20:19:13 +0700

    Add categories to the report
    This is to mitigate a little the inability of allure-specflow to distinguish
    between test and product defects on its own.

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Example of usage Allure Report with SpecFlow
 This project uses SpecFlow 3 and .NET 7.0.
 
 The generated report is available here: (https://allure-examples.github.io/allure-specflow).
+
+> [!NOTE]
+> The report has manually defined categories to help distinguish between test
+> and product defects. Allure-specflow doesn't do that on its own because of
+> (https://github.com/allure-framework/allure-csharp/issues/399).
---
+++ tests/Allure.Examples.SpecFlow3/Hooks.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Text;
+using Allure.Net.Commons;
+using TechTalk.SpecFlow;
+
+namespace Allure.Examples.SpecFlow3;
+
+[Binding]
+public static class Hooks
+{
+    const string CATEGORIES_PATH =
+        "Allure.Examples.SpecFlow3.Resources.categories.json";
+    static readonly Assembly currentAssembly = Assembly.GetExecutingAssembly();
+
+    [AfterTestRun]
+    public static async Task CopyCategoriesToAllureDirectory()
+    {
+        using var stream =
+            currentAssembly.GetManifestResourceStream(CATEGORIES_PATH);
+        if (stream is null)
+        {
+            return;
+        }
+
+        using var reader = new StreamReader(stream);
+        var directory = AllureLifecycle.Instance.AllureConfiguration.Directory;
+        if (!Directory.Exists(directory))
+        {
+            return;
+        }
+        var outputPath = Path.Combine(directory, "categories.json");
+        var content = await reader.ReadToEndAsync();
+        await File.WriteAllTextAsync(outputPath, content, Encoding.UTF8);
+    }
+}
